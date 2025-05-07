02d4169c38444ac06487180a6775336e8d90e0e7
andreirhamni09 <andreirhamni09@gmail.com>
2025-05-07 11:24:25 +0700

    Update Controller

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerTesting;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tes_db', [ControllerTesting::class, "index"]);
